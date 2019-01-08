5431778e15bc01a81b8794d23aee0b508dad86b3
muthuramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-01-08 20:06:41 +0530

    Update mupltiples.py

--- mupltiples.py
+++ mupltiples.py
@@ -1,8 +1,8 @@
 a=int(input())
 li=[]
 for i in range(1,6):
-    f=a*i
-    li.append(f)
-for i in range(0,a+1):
-		print(li[i],end=" ")
+	f=a*i
+	li.append(f)
+for i in range(0,a):
+	print(li[i],end=" ")
  
