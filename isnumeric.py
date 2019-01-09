de6712c7b270aacd9b5c1ef4e72537b71e78247b
muthuramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-01-09 12:27:28 +0530

    Update isnumeric.py

--- isnumeric.py
+++ isnumeric.py
@@ -4,4 +4,4 @@ if a.isnumeric():
 	print("yes")
 else:
 	print("No")
-https://github.com/muthuramasamy/muthu/blob/master/isnumeric.pyhttps://github.com/muthuramasamy/muthu/blob/master/isnumeric.py
+
