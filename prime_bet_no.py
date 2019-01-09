07ececb2bf50a16f49d49bf566a1c1096a61b08b
muthuramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-01-09 10:33:44 +0530

    Update prime_bet_no.py

--- prime_bet_no.py
+++ prime_bet_no.py
@@ -1,15 +1,21 @@
-k,t=map(int,input().split(" "))
 li=[]
-for i in range(k+1,t):
-	if i>1:
-		for j in range(2,i):
-			if i%j ==0:
-				break
-		else:
-			li.append(i)
-for i in range (0,len(li)):
-	if (li[i]==i):
+def ch(b):
+	a1=str(b)
+	tlen=len(a1)
+	amst=0
+	b1=b
+	while(b>0):
+		t=b%10
+		t1=(t**tlen)
+		amst=amst+t1
+		b=b//10
+	if(b1==amst):
+		li.append(amst)
+l,k=map(int,input().split(" "))
+for i in range(l+1,k):
+	ch(i)
+for i in range(len(li)):
+	if(i==len(li)-1):
 		print(li[i],end="")
 	else:
 		print(li[i],end=" ")
-  
