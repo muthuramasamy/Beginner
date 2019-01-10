2db8eadf37c9b94b2abb4e67cae4c2736fa8cb52
muthuramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-01-10 16:13:42 +0530

    Create player_Roman_convert.py

---
+++ player_Roman_convert.py
@@ -0,0 +1,6 @@
+l=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+n=input()
+for i in range(0,len(l)):
+	if n==l[i]:
+		print(i+1)
+		
