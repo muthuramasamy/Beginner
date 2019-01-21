b1aec5e4d2fd64a7bdd85c72930c7813e43c0e7e
muthuramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-01-21 19:10:57 +0530

    Create 58.check_k_value.py

---
+++ 58.check_k_value.py
@@ -0,0 +1,11 @@
+# your code goes here
+count=0
+n,k=map(int,input().split())
+p=list(map(int,input().split()))
+for i in range(0,len(p)):
+	if k==p[i]:
+		count=count+1
+if count==1:
+	print('yes')
+else:
+	print('no')
