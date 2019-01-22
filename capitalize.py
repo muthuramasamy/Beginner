9b0ba28b7bc7bd742fc653d00ad17354c59e5621
muthuramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-01-22 19:46:27 +0530

    Update capitalize.py

--- capitalize.py
+++ capitalize.py
@@ -1,4 +1,12 @@
 # your code goes here
 n=list(map(str,input().split()))
+li=[]
 for i in range(len(n)):
-	print(n[i].capitalize(),end=" ")
+	k=n[i].capitalize()
+	li.append(k)
+for i in range(0,len(li)):
+	if i==len(li)-1:
+		print(li[i],end="")
+	else:
+		print(li[i],end=" ")
+	
