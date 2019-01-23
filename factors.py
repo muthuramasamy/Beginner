3903d22a34a81fb398ef2bb55a9669561469552d
muthuramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-01-23 02:42:22 -0800

    Create factors.py

---
+++ factors.py
@@ -0,0 +1,13 @@
+# your code goes here
+n=int(input())
+li=[]
+for i in range(1,n):
+	if n%i==0:
+		li.append(i)
+	else:
+		continue
+for i in range(0,len(li)):
+	if i==len(li)-1:
+		print(li[i],end='')
+	else:
+		print(li[i],end=' ')
